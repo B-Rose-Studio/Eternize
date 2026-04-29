3a4273c0a5b47a246f01442616a69ab75dfacc7d
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-04-28 21:36:18 -0300

    feat: add migrations for Database and Run Local DB for Makefile

---
+++ migrations/01_create_tables.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS signatures (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    max_sections INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id TEXT PRIMARY KEY,
+    type TEXT NOT NULL CHECK(type IN ('Client', 'Admin')),
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone TEXT,
+    active INTEGER NOT NULL DEFAULT 1,
+    birthdate TEXT
+);
+
+CREATE TABLE IF NOT EXISTS customize_pages (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    title TEXT NOT NULL,
+    purchased_in TEXT NOT NULL,
+    renewed_in TEXT,
+    active INTEGER NOT NULL DEFAULT 1,
+    user_id TEXT NOT NULL, --FK
+    signature_id TEXT NOT NULL,  --FK
+
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (signature_id) REFERENCES signatures(id)
+);
+
+CREATE TABLE IF NOT EXISTS sections (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    page_id TEXT NOT NULL, --FK
+
+    FOREIGN KEY (page_id) REFERENCES customize_pages(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS propertys (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    value TEXT,
+    section_id TEXT NOT NULL, --FK
+
+    FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE
+);
---
+++ migrations/02_insert_plan_and_user.sql
@@ -0,0 +1,14 @@
+INSERT OR IGNORE INTO signatures (id, name, price, max_sections)
+VALUES ('7b1897e0-466d-4b82-9654-2c0692557e84', 'Basic', 25.00, 5);
+
+-- (password: admin123) Bcrypt with 12 rounds
+INSERT OR IGNORE INTO users (id, type, email, password, first_name, last_name, active)
+VALUES (
+    'a1b2c3d4-e5f6-4a5b-bc6d-7e8f9a0b1c2d',
+    'Admin',
+    'admin@bluerosestudio.com',
+    '$2a$12$N9qx1y03Z41D7Q7g1bV5U.rD/61P.k82Q/cK./gS1q81y32Z.496K',
+    'Admin',
+    'Blue Rose',
+    1
+);
